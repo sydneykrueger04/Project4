a0cb7b94bf9b96b256f9b93fadd01248ed82d63a
NJBurr <nburr@cedarville.edu>
2025-11-02 18:02:51 -0500

    need to be able to back track once we get to the end, as well as a few other things to keep track of as we search

--- rushHour.cpp
+++ rushHour.cpp
@@ -23,17 +23,31 @@ struct VehicleInfo {
 	int col;
 };
 
+// ? this converts our 2d array into a string so we can use it as a key in our hash map
+string vectorToString(vector<vector<int>> board) {
+	string keyString = "";
+	for (int i=0; i<6; i++) {
+		for (int j=0; j<6; j++) {
+			keyString += to_string(board[i][j]) + ',';
+		}
+	}
+
+	return keyString;
+}
+
 bool checkValid(int targetRow, int targetCol, vector<vector<int>>& board) {
 	return targetRow < 6 && targetRow >= 0 && targetCol < 6 && targetCol >= 0 && board[targetRow][targetCol] == -1;
 }
 
-void checkPossibleMoves(vector<vector<int>>& board, int row, int col, int len, vector<VehicleInfo>& newVehicleInfo, char direction, queue<vector<vector<int>>> boardStatesQueue, 
+void checkPossibleMoves(vector<vector<int>>& board, int row, int col, int len, vector<VehicleInfo>& newVehicleInfo, char direction, queue<vector<vector<int>>>& boardStatesQueue, 
 	unordered_map<string, vector<VehicleInfo>>& boardInformation, unordered_map<string, vector<string>>& listOfMoves, int carNumber) {
 	// ? if this is true we can move forward
 	int count = 0;
 	bool valid;
 	vector<vector<int>> newBoardState = board;
 	string key;
+	string oldKey;
+	unordered_map<string, bool> visited;
 
 	if (direction == 'L') {
 		valid = checkValid(row, col-1, board);
@@ -51,43 +65,51 @@ void checkPossibleMoves(vector<vector<int>>& board, int row, int col, int len, v
 
 		// ? move the vehicle to the direction by one
 		newBoardState[row][col] = carNumber;
-		if (direction == 'L') newBoardState[row][col+len] = -1, newVehicleInfo[carNumber].col = col;
-		else if (direction == 'R') newBoardState[row][col-len] = -1, newVehicleInfo[carNumber].col = col+len-1;
-		else if (direction == 'U') newBoardState[row+len][col] = -1, newVehicleInfo[carNumber].row = row;
-		else newBoardState[row-len][col] = -1, newVehicleInfo[carNumber].row = row-len+1;
+		if (direction == 'L') {
+			newBoardState[row][col-1] = carNumber;
+			newBoardState[row][col+len-1] = -1;
+			newVehicleInfo[carNumber].col = col-1;
+			valid = checkValid(newVehicleInfo[carNumber].row, newVehicleInfo[carNumber].col-1, newBoardState);
+		}
+		else if (direction == 'R') {
+			newBoardState[row][col+len] = carNumber;
+			newBoardState[row][col] = -1;
+			newVehicleInfo[carNumber].col = col+1;
+			valid = checkValid(newVehicleInfo[carNumber].row, newVehicleInfo[carNumber].col+len, newBoardState);
+
+			// todo: check if the red car can get all the way to [2][5]
+			if (carNumber == 0 && newVehicleInfo[carNumber].col == 5) {
+				// ! this is solution, print out path
+			}
+		}
+		else if (direction == 'U') {
+			newBoardState[row-1][col] = carNumber;
+			newBoardState[row+len-1][col] = -1;
+			newVehicleInfo[carNumber].row = row-1;
+			valid = checkValid(newVehicleInfo[carNumber].row-1, newVehicleInfo[carNumber].col, newBoardState);
+		}
+		else {
+			newBoardState[row+len][col] = carNumber;
+			newBoardState[row][col] = -1;
+			newVehicleInfo[carNumber].row = row+1;
+			valid = checkValid(newVehicleInfo[carNumber].row+len, newVehicleInfo[carNumber].col, newBoardState);
+		}
 
 		// ? convert the 2d vector to a string to use as a key
 		key = vectorToString(newBoardState);
-
-		// ? check validity of next move
-		if (direction == 'L') newBoardState[row][col+len] = col--;
-		else if (direction == 'R') newBoardState[row][col-len] = col++;
-		else if (direction == 'U') newBoardState[row+len][col] = row--;
-		else newBoardState[row-len][col] = row++;
-
-		valid = checkValid(row, col, newBoardState);
 	}
 
 	// ? assign the vehicle info to this state to the string of this state in a hash map
 	boardInformation[key] = newVehicleInfo;
 
 	// ? assign the move you made here in relation to this new state
-	listOfMoves[key].push_back((to_string(carNumber) + " " + to_string(count) + " R"));
+	listOfMoves[key].push_back((to_string(carNumber) + " " + to_string(count) + direction));
 
 	// ? push new state onto the queue
 	boardStatesQueue.push(newBoardState);
-}
 
-// ? this converts our 2d array into a string so we can use it as a key in our hash map
-string vectorToString(vector<vector<int>> board) {
-	string keyString = "";
-	for (int i=0; i<6; i++) {
-		for (int j=0; j<6; j++) {
-			keyString += to_string(board[i][j]);
-		}
-	}
-
-	return keyString;
+	// ? this lets us know we have already seen this board state
+	visited[key] = true;
 }
 
 // ? we pass in the number of vehicles, info about each vehicle, and the current board state holding each vehicle
@@ -102,24 +124,30 @@ void puzzleSolve(int numOfVehicles, const vector<VehicleInfo>& vehicles, vector<
 	string key;
 	vector<VehicleInfo> newVehicleInfo;
 	vector<vector<int>> newBoardState;
+	unordered_map<string, vector<vector<int>>> parent; // ? this will allow us to assign parents to then have us backtrack at the end
 	// ! this goes depth 1 of checking vehicles for their possible moves
-	for (int i=0; i<numOfVehicles; i++) {
+	while (!boardStatesQueue.empty()) {
 		board = boardStatesQueue.front(); // ? getting the board at the front of the queue
 		key = vectorToString(board);
 		newVehicleInfo = boardInformation[key];
-		if (vehicles[i].orien == 'h') {
-			// check left
-			checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col-1, newVehicleInfo[i].length, newVehicleInfo, 'L', boardStatesQueue, boardInformation, listOfMoves, i);
-			
-			// check right
-			checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col-1, newVehicleInfo[i].length, newVehicleInfo, 'R', boardStatesQueue, boardInformation, listOfMoves, i);
-		} else {
-			// check up
-			checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col-1, newVehicleInfo[i].length, newVehicleInfo, 'U', boardStatesQueue, boardInformation, listOfMoves, i);
+		for (int i=0; i<numOfVehicles; i++) {
+			if (vehicles[i].orien == 'h') {
+				// check left
+				checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col, newVehicleInfo[i].length, newVehicleInfo, 'L', boardStatesQueue, boardInformation, listOfMoves, i);
+				
+				// check right
+				checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col, newVehicleInfo[i].length, newVehicleInfo, 'R', boardStatesQueue, boardInformation, listOfMoves, i);
+			} else {
+				// check up
+				checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col, newVehicleInfo[i].length, newVehicleInfo, 'U', boardStatesQueue, boardInformation, listOfMoves, i);
 
-			//check down
-			checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col-1, newVehicleInfo[i].length, newVehicleInfo, 'D', boardStatesQueue, boardInformation, listOfMoves, i);
+				//check down
+				checkPossibleMoves(board, newVehicleInfo[i].row, newVehicleInfo[i].col, newVehicleInfo[i].length, newVehicleInfo, 'D', boardStatesQueue, boardInformation, listOfMoves, i);
+			}
 		}
+
+		// ? pop off the first element in the queue because we have finished that breadth level
+		boardStatesQueue.pop();
 	}
 }
 
@@ -174,7 +202,6 @@ int main() {
 		if (type == "car") {
 			length = 2;
 		}
-
 		else {
 			length = 3;
 		}
@@ -227,6 +254,9 @@ int main() {
 		}
 		cout << "\n";
 	}
+
+	// call the solve function
+	puzzleSolve(numVehicle, vehicles, board);
 
 	return 0;
 }
